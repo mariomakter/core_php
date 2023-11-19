9dcec9d9daf3d30baf2587a585be9686f0a3eb23
mariomakter <mariom9267idb@gmail.com>
2023-11-20 01:31:45 +0600

    real life example is inheritance

--- inheritance_ex-2.php
+++ inheritance_ex-2.php
@@ -26,5 +26,18 @@ class Tringle extends Shape{
     }
 
 }
+class Rectangla extends shape{
+    private $a, $b;
+    public function __construct($a, $b){
+        $this->a= $a;
+        $this->b = $b;
+        parent::__construct("Rectangle");
+    }
+    public function calculatArea(){
+        $this->area = $this->a * $this->b;
+    }
+}
 $tringle = new Tringle(2,3,2);
 $tringle->getArea();
+$rectangle = new Rectangla(3,4);
+$rectangle->getArea();
